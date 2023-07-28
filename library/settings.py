54961fafb67f9d472947cc404e56ec34b2b467ec
Shakhriyor <shaxriyorsharofov911@gmail.com>
2023-07-28 19:05:10 +0500

    *add

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
